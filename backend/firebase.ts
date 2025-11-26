b238f5bc32f31dad3dc3b58908b16e9526e188b5
Mindi Hu <mindimhu@gmail.com>
2025-11-26 15:30:48 -0500

    made a private key to connect to database

--- backend/firebase.ts
+++ backend/firebase.ts
@@ -1,25 +1,50 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+require('dotenv').config({ path: '../.env' }); // <-- Load the .env file from the root directory
+
+import * as admin from 'firebase-admin';
+
+import { initializeApp, applicationDefault, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from "firebase/auth";
 
-// Our web app's Firebase configuration (example)
-const firebaseConfig = {
-  apiKey: "AIzaSyCHla7lE-lgqKpYIaNbs4jlg2bcuXHOSWI",
-  authDomain: "cornell-geoguesser-a5fdd.firebaseapp.com",
-  projectId: "cornell-geoguesser-a5fdd",
-  storageBucket: "cornell-geoguesser-a5fdd.firebasestorage.app",
-  messagingSenderId: "657767365273",
-  appId: "1:657767365273:web:29f6facecfebf671d51608",
-  measurementId: "G-SGYHCTBYY8"
+const serviceAccount: admin.ServiceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  // The private key must be specifically parsed to interpret the \n as a real newline
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+  credential: cert(serviceAccount),
+});
+const db = getFirestore();
 
-// Initialize Firestore
-export const db = getFirestore(app);
-//db logic here
-
+export { db };
 export const auth = getAuth();
+
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { getAuth } from "firebase/auth";
+
+// // Our web app's Firebase configuration (example)
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCHla7lE-lgqKpYIaNbs4jlg2bcuXHOSWI",
+//   authDomain: "cornell-geoguesser-a5fdd.firebaseapp.com",
+//   projectId: "cornell-geoguesser-a5fdd",
+//   storageBucket: "cornell-geoguesser-a5fdd.firebasestorage.app",
+//   messagingSenderId: "657767365273",
+//   appId: "1:657767365273:web:29f6facecfebf671d51608",
+//   measurementId: "G-SGYHCTBYY8"
+// };
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// // Initialize Firestore
+// export const db = getFirestore(app);
+// //db logic here
+
+// export const auth = getAuth();
+
+
+
 
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
