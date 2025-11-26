2b222eb8ebac3eec486415d986f11b449a8807d7
ClaireOka <134355428+ClaireOka@users.noreply.github.com>
2025-11-26 18:21:33 -0500

    score!

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -28,6 +28,42 @@ const ResultsPage = () => {
         shadowAnchor: [20, 50],        // optional
     });
 
+    const getScore = (dist: number) => {
+        if (dist > 3000) {
+            return 0;
+        }
+        else if (dist > 2000) {
+            return (10 - (dist - 2000)/100).toFixed(2);
+        }
+        else if (dist > 1200) {
+            return (10 - (dist-1200)/80 + 10).toFixed(2) ;
+        }
+        else if (dist > 700) {
+            return (10 - (dist-700)/50  + 20).toFixed(2);
+        }
+        else if (dist > 350) {
+            return (10 - (dist-350)/35 + 30).toFixed(2);
+        }
+        else if (dist > 200) {
+            return (10 - (dist-200)/15 + 40).toFixed(2);
+        }
+        else if (dist > 100) {
+            return (10 - (dist-100)/10 + 50).toFixed(2);
+        }
+        else if (dist > 50) {
+            return (10 - (dist-50)/5 + 60).toFixed(2);
+        }
+        else if (dist > 25) {
+            return (10 - (dist-25)/2.5 + 70).toFixed(2);
+        }
+        else if (dist > 10) {
+            return (10 - (dist - 10)/1.5 + 80).toFixed(2);
+        }
+        else {
+            return (10 - dist+90).toFixed(2);
+        }
+    }
+
     // Update user score
     fetch("/backend/api/users/update-score", {
         method: "PUT",
@@ -73,7 +109,7 @@ const ResultsPage = () => {
                 
 
             <p>Your distance from goal: {distMeters.toFixed(2)} meters</p>
-            <p>Points Earned: {/*some points calc func*/} </p>
+            <p>Points Earned: {getScore(distMeters)} </p>
             <p>Total Points: {/*access user total points*/} </p>
 
             <div style={{ display: "flex", gap: "20px", justifyContent: "center" }}>
