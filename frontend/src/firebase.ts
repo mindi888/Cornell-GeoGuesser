65bfa3e26717be4d96403c40492465b69ff00d30
Mindi Hu <mindimhu@gmail.com>
2026-01-15 01:04:39 -0500

    Revert "attempt again"
    This reverts commit 08d000177ba69ee74c4b75a8b60b5095f7a27f19.

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -18,7 +18,7 @@ export const app = initializeApp(firebaseConfig);
 // Initialize Firestore
 export const db = getFirestore(app);
 
-export const auth = getAuth(app);
+export const auth = getAuth();
 
 // // Import the functions you need from the SDKs you need
 // import { initializeApp } from "firebase/app";
