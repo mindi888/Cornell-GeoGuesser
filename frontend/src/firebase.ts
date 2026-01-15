07c747a035d9f39073a8c8a93fe6187ed707c6f5
Mindi Hu <mindimhu@gmail.com>
2026-01-15 01:04:27 -0500

    Revert "Update firebase.ts"
    This reverts commit 22f39386550890c78951978a33f11e3a3fd81da7.

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -12,7 +12,6 @@ const firebaseConfig = {
   appId: "1:657767365273:web:29f6facecfebf671d51608",
   measurementId: "G-SGYHCTBYY8"
 };
-console.log(firebaseConfig)
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
