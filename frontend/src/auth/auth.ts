1ce1ebb9707a721ae5fbcf3b133eb7757adf6fd3
Mindi Hu <mindimhu@gmail.com>
2026-01-15 23:28:35 -0500

    added to serivceAccount

--- frontend/src/auth/auth.ts
+++ frontend/src/auth/auth.ts
@@ -71,27 +71,6 @@ export const signIn = async () => {
 };
 
 
-// export const signIn = async () => {
-//   try {
-//     const result = await signInWithPopup(auth, provider);
-//     const credential = GoogleAuthProvider.credentialFromResult(result);
-//     const token = credential?.accessToken;
-//     const user = result.user;
-
-//     return { token, user };
-//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   } catch (err: any) {
-//     const code = err.code;
-//     const message = err.message;
-//     const email = err.customData.email;
-
-//     console.log(
-//       `An error ${code} occurred when logging user with email: ${email} with message: ${message}`
-//     );
-//     return null;
-//   }
-// };
-
 export const signOut = async () => {
   await auth.signOut();
 };
